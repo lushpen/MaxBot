94730b115152b7321d931dcabd608ca73dac923e
lushpen <99793708+lushpen@users.noreply.github.com>
2022-07-11 08:49:25 +0300

    Add files via upload

--- max.js
+++ max.js
@@ -410,6 +410,7 @@ bot.on("callback_query", async (ctx) => {
   ) {
     if (ctx.update.callback_query.message.message_id) {
       //bestResults++;
+      bestResults = bestResults ?? 0; // fix for NAN for new users
       bestResults = ctx.db[chatID].bestResults + 1;
       bot.context.db[chatID] = Object.assign(ctx.db[chatID], {
         bestResults,
